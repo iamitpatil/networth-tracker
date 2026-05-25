3e03b04d01ac06c5f0b34ea903860085d87687dc
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2026-05-25 17:17:53 +0530

    Use official Protean logo from media kit (cdn.prod.website-files.com)

--- web/src/components/AccountsHub.jsx
+++ web/src/components/AccountsHub.jsx
@@ -18,7 +18,7 @@ const getBrokerLogo = (name) => {
 }
 
 const NPS_CRAS = [
-  { value: 'PROTEAN', label: 'Protean (NSDL)', logo: 'https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://proteantech.in&size=128' },
+  { value: 'PROTEAN', label: 'Protean (NSDL)', logo: 'https://cdn.prod.website-files.com/66cc3023e479316bd776826c/69270cdb3c806fc344b09dec_Protean%20logo_Horizontal-02%201.jpg' },
   { value: 'KFINTECH', label: 'KFintech (Karvy)', logo: 'https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://kfintech.com&size=128' },
   { value: 'CAMS', label: 'CAMS', logo: 'https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://camsonline.com&size=128' },
 ]
